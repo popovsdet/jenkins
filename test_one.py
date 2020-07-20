e9e2ed829dfdfca143689e99d610628ef4e03002
popovsdet <popovsdet@gmai.com>
2020-07-20 12:06:04 -0700

    grid is done

--- test_one.py
+++ test_one.py
@@ -1,4 +1,14 @@
-print("Hello")
-print("one more!")
-for x in range(3):
-    print(x)
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+
+caps = DesiredCapabilities.CHROME
+
+driver = webdriver.Remote(command_executor='http://54.183.169.175:4444/wd/hub', desired_capabilities=caps)
+
+driver.get('https://www.google.com/')
+print("Open Google")
+sleep(5)
+
+driver.quit()
