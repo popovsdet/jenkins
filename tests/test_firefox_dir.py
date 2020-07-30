4170a3ee2b110f8eb40862babd8ebd2ddaf43828
popovsdet <popovsdet@gmai.com>
2020-07-29 19:45:37 -0700

    New file

--- tests/test_firefox_dir.py
+++ tests/test_firefox_dir.py
@@ -12,7 +12,7 @@ def test_two():
     }
 
     driver = webdriver.Remote(
-        command_executor="http://18.144.8.11:4444/wd/hub",
+        command_executor="http://18.144.86.211:4444/wd/hub",
         desired_capabilities=capabilities)
 
     driver.get('https://www.google.com/')
